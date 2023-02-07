a74ba109fa6254b33d7f2fd2718e6d1985ddc251
Elijah-Hensel <elijah.j.hensel@gmail.com>
2023-02-06 22:19:14 -0600

    environment variable for api key:

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,4 @@
-import * as dotenv from "dotenv" // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
-
-const KEY = 'dbQ1W7CDCrQ4pLA92RRSiQRZZFK276hK'
+const KEY = process.env.REACT_APP_GIPHY_API_KEY
 
 export const getInitialGifs = async () => {
   try {
