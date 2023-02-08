768298088016db28c22a2bff0fe108205f587345
Elijah-Hensel <elijah.j.hensel@gmail.com>
2023-02-07 20:46:43 -0600

    actions tests

--- src/tests/utils/actions.test.js
+++ src/tests/utils/actions.test.js
@@ -1,7 +1,20 @@
-// describe("Properly sets and returns gifs by query parameter", () => {
-//   it(`returns gifs related to 'fun'`, async () => {
-//     const queryString = "fun"
-//     const
-//     await expect(gifs).toBe("Request failed with status code 404")
-//   })
-// })
+import { setGifsByQuery, setInitialGifs } from '../../utils/actions';
+
+describe("Properly sets and returns gifs after search action", () => {
+  it(`returns gifs related to 'funny'`, async () => {
+    const queryString = "funny"
+    const action = ({data, pagination}) => ({ data, pagination })
+    const { data } = await setGifsByQuery(queryString, action)
+    expect(data.length).toEqual(20)
+  })
+})
+
+describe("Properly sets initial gifs", () => {
+  it(`returns 3 initial trending gifs`, async () => {
+    const action = ({data, pagination}) => ({ data, pagination })
+    const { data } = await setInitialGifs(action)
+    expect(data.length).toEqual(3)
+  })
+})
+
+
--- src/utils/actions.js
+++ src/utils/actions.js
@@ -2,10 +2,10 @@ import { getInitialGifs, getGifsBySearchQuery } from "../api"
 
 export const setGifsByQuery = async (query, action) => {
         const { data, pagination } = await getGifsBySearchQuery(query)
-        action({ data, pagination })
+        return action({ data, pagination })
       }
 
 export const setInitialGifs = async (action) => {
         const { data, pagination } = await getInitialGifs()
-        action({ data, pagination })
+        return action({ data, pagination })
       }
